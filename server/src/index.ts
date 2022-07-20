5d02af4a9daff43214fb109eb6163fb91274dbce
Andrey <tankistmolodoy@gmail.com>
2022-07-20 19:06:13 +0300

    two folder update

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,9 +13,9 @@ app.use("/api/auth/", require("./routes/auth.routes"));
 
 app.use("/api", jsonServer.router(path.join(__dirname, "json-server", "db.json")));
 
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
+app.use(express.static(path.join(__dirname, "..", "..", "client", "build")));
 app.get("*", (req: any, res: any) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "..", "client", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || config.get("port") || 5000;
